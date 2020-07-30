256a519c24836a4a52ab9cb1b10d054c46eafff2
shangweikun <shangweikun@yeah.net>
2020-07-30 23:36:02 +0800

    LeetCode437's solution

---
+++ src/main/java/com/again1/www/LeetCode437.java
@@ -0,0 +1,56 @@
+package com.again1.www;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class LeetCode437 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+
+        Deque stack = new LinkedList();
+        int result = 0;
+
+        public int pathSum(TreeNode root, int sum) {
+
+            if (root == null) return 0;
+
+            calculateSum(root, sum);
+            pathSum(root.left, sum);
+            pathSum(root.right, sum);
+
+            return result;
+        }
+
+        private void calculateSum(TreeNode root, int sum) {
+            if (root == null)
+                return;
+            sum -= root.val;
+            if (sum == 0) {
+                result++;
+            }
+            calculateSum(root.left, sum);
+            calculateSum(root.right, sum);
+            sum += root.val;
+        }
+    }
+}
