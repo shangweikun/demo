a52146469a2e8142e2136a76d720036ec8f37b41
shangweikun <shangweikun@yeah.net>
2020-07-30 23:50:50 +0800

    Solution for 437

--- src/main/java/com/again1/www/LeetCode437.java
+++ src/main/java/com/again1/www/LeetCode437.java
@@ -53,4 +53,27 @@ public class LeetCode437 {
             sum += root.val;
         }
     }
+
+    public int pathSum(TreeNode root, int sum) {
+        return pathSum(root, sum, new int[1000], 0);
+    }
+
+    public int pathSum(TreeNode root, int sum, int[] array/*保存路径*/, int p/*指向路径终点*/) {
+        if (root == null) {
+            return 0;
+        }
+        int tmp = root.val;
+        int n = root.val == sum ? 1 : 0;
+        for (int i = p - 1; i >= 0; i--) {
+            tmp += array[i];
+            if (tmp == sum) {
+                n++;
+            }
+        }
+        array[p] = root.val;
+        int n1 = pathSum(root.left, sum, array, p + 1);
+        int n2 = pathSum(root.right, sum, array, p + 1);
+        return n + n1 + n2;
+    }
+
 }
