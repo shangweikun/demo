2821fc7d164f1060c7e6426a8b9cf0412f15690a
shangweikun <shangweikun@yeah.net>
2020-09-08 18:51:32 +0800

    leetcode100

---
+++ src/main/java/com/again1/www/App.java
@@ -0,0 +1,11 @@
+package com.again1.www;
+
+import javax.ws.rs.POST;
+
+public class App {
+
+	@POST
+	 public void test(){
+
+	 }
+}
---
+++ src/main/java/com/again1/www/LeetCode100.java
@@ -0,0 +1,38 @@
+package com.again1.www;
+
+public class LeetCode100 {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution {
+		public boolean isSameTree(TreeNode p, TreeNode q) {
+			if (p == null || q==null) {
+				return q == null && p ==null;
+			}
+
+			if (p.val == q.val) {
+				return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+			} else {
+				return false;
+			}
+
+		}
+	}
+}
